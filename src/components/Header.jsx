a64db9a7d6bba800137cc4355e5733198cf25831
Angelo Brillantes <arbrillantes@fit.edu.ph>
2026-03-21 00:01:30 +0800

    Updated page contents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="bg-dark text-white p-3 text-center">
-      <h1>My E-Commerce Store</h1>
+      <h1>Hi</h1>
       <p>Find the best products here!</p>
     </header>
   );
